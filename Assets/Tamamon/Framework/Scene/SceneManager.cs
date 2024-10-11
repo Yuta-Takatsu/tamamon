ffd154c8b6e6d0e3a5630847d1b75b2567416820
DESKTOP-MET2H7A\ytaka <y.takatsu.1012@gmail.com>
2024-10-12 06:40:53 +0900

    FadeManager追加

--- Assets/Tamamon/Framework/Scene/SceneManager.cs
+++ Assets/Tamamon/Framework/Scene/SceneManager.cs
@@ -6,24 +6,19 @@ namespace Framework
 {
     public class SceneManager : MonoBehaviourSingleton<SceneManager>
     {
-        [SerializeField]
-        private CanvasGroup m_fadePanel = default;
-
-        private bool m_isFade = false;
-        private readonly float FadeTime = 1.5f;
 
         public async UniTask LoadSceneAsync(string name, UnityEngine.SceneManagement.LoadSceneMode mode = UnityEngine.SceneManagement.LoadSceneMode.Single, bool isFade = true)
         {
-            if (isFade) await FadeIn();
+            if (isFade) await FadeManager.Instance.FadeIn();
 
             await UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(name, mode);
 
-            await SceneManager.Instance.FadeOut();
+            await FadeManager.Instance.FadeOut();
         }
 
         public async UniTask UnLoadSceneAsync(string name, bool isFade = true)
         {
-            if (isFade) await FadeIn();
+            if (isFade) await FadeManager.Instance.FadeIn();
 
             await UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(name);
             await Resources.UnloadUnusedAssets();
@@ -46,36 +41,6 @@ namespace Framework
                 }
             }
             return null;
-        }
-
-        public async UniTask FadeIn()
-        {
-            m_isFade = true;
-            m_fadePanel.gameObject.SetActive(true);
-            m_fadePanel.alpha = 0f;
-            m_fadePanel.DOFade(1f, FadeTime)
-                .OnComplete(() =>
-            {
-                m_fadePanel.alpha = 1f;
-                m_isFade = false;
-            });
-
-            await UniTask.WaitWhile(() => m_isFade);
-        }
-
-        public async UniTask FadeOut()
-        {
-            m_isFade = true;
-            m_fadePanel.alpha = 1f;
-            m_fadePanel.DOFade(0f, FadeTime)
-                .OnComplete(() =>
-                {
-                    m_fadePanel.gameObject.SetActive(false);
-                    m_fadePanel.alpha = 0f;
-                    m_isFade = false;
-                });
-
-            await UniTask.WaitWhile(() => m_isFade);
         }
     }
 }
