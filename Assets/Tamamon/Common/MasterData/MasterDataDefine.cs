af0fe1547196cdace735c80b1840b0a1a00803ee
naonosuke8267 <naonosuke8267@gmail.com>
2024-10-12 19:23:33 +0900

    マスターデータ系　namespace隔離
    MasterDataへ移行

--- Assets/Tamamon/Common/MasterData/MasterDataDefine.cs
+++ Assets/Tamamon/Common/MasterData/MasterDataDefine.cs
@@ -2,72 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class MasterDataDefine
-{
-	public static string MASTER_MONSTERS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRemKLn1RaIW-u-JFw5vE9z2KIztKyyIRoBmdPGmznQJFzKzTZACVTBJ-bpIZ2nYHg5RRSv_QcDNH_t/pub?gid=0&single=true&output=csv";
-
-	/// <summary>
-    /// MasterDataで使用する経験値テーブル
-    /// </summary>
-	public enum masEnum_expTable
+namespace Framework.MasterData {
+	public static class MasterDataDefine
 	{
-		FASTEST=0,		// 俗にいう60テーブル　最速
-		QUICK,			// 80テーブル　早め
-		NORMAL,			// 100テーブル　ふつう
-		PRECOCIOUS,		// 105テーブル　早熟
-		LATEBLOOM,		// 125テーブル　大器晩成
-		LATEST,			// 164テーブル　つくんな
-	}
+		public static string MASTER_MONSTERS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRemKLn1RaIW-u-JFw5vE9z2KIztKyyIRoBmdPGmznQJFzKzTZACVTBJ-bpIZ2nYHg5RRSv_QcDNH_t/pub?gid=0&single=true&output=csv";
 
-	/// <summary>
-    /// MasterDataで使用するタマゴグループ
-    /// </summary>
-	public enum masEnum_eggGroup
-	{
-		NONE=0,			// なし
-		KAIJU,			// かいじゅう
-		MINERAL,		// 鉱物
-		PLANT,			// 植物
-		WATER1,			// 水中１
-		WATER2,			// 水中２
-		WATER3,			// 水中３
-		DRAGON,			// ドラゴン
-		FLYING,			// 飛行
-		HUMANOID,		// 人型
-		UNFORMED,		// 不定形
-		INCECT,			// 虫
-		FAILY,			// 妖精
-		GROUND,			// 陸上
-		JOKER,			// なんでも
-		NOTHING,		// タマゴ未発見
-	}
+		/// <summary>
+		/// MasterDataで使用する経験値テーブル
+		/// </summary>
+		public enum masEnum_expTable
+		{
+			FASTEST=0,		// 俗にいう60テーブル　最速
+			QUICK,			// 80テーブル　早め
+			NORMAL,			// 100テーブル　ふつう
+			PRECOCIOUS,		// 105テーブル　早熟
+			LATEBLOOM,		// 125テーブル　大器晩成
+			LATEST,			// 164テーブル　つくんな
+		}
 
-	/// <summary>
-    /// MasterDataで使用するタイプ
-    /// </summary>
-	public enum masEnum_tamamonType
-	{
-		NONE=0,			// なし
-		NORMAL,			// ノーマル
-		FIRE,			// ほのお
-		WATER,			// みず
-		GLASS,			// くさ
-		ELECTLIC,		// でんき
-		INCECT,			// むし
-		FLYING,			// ひこう
-		FIGHTER,		// かくとう
-		ROCK,			// いわ
-		METAL,			// はがね
-		GROUND,			// じめん
-		POISON,			// どく
-		HEEL,			// あく
-		PSYCHIC,		// エスパー
-		GOAST,			// ゴースト
-		DRAGON,			// ドラゴン
-		FAILY,			// フェアリー
-		ICE,			// こおり
-		LEGEND,			// レジェンド？
-		STELLA,			// ステラ？
-	}
+		/// <summary>
+		/// MasterDataで使用するタマゴグループ
+		/// </summary>
+		public enum masEnum_eggGroup
+		{
+			NONE=0,			// なし
+			KAIJU,			// かいじゅう
+			MINERAL,		// 鉱物
+			PLANT,			// 植物
+			WATER1,			// 水中１
+			WATER2,			// 水中２
+			WATER3,			// 水中３
+			DRAGON,			// ドラゴン
+			FLYING,			// 飛行
+			HUMANOID,		// 人型
+			UNFORMED,		// 不定形
+			INCECT,			// 虫
+			FAILY,			// 妖精
+			GROUND,			// 陸上
+			JOKER,			// なんでも
+			NOTHING,		// タマゴ未発見
+		}
 
+		/// <summary>
+		/// MasterDataで使用するタイプ
+		/// </summary>
+		public enum masEnum_tamamonType
+		{
+			NONE=0,			// なし
+			NORMAL,			// ノーマル
+			FIRE,			// ほのお
+			WATER,			// みず
+			GLASS,			// くさ
+			ELECTLIC,		// でんき
+			INCECT,			// むし
+			FLYING,			// ひこう
+			FIGHTER,		// かくとう
+			ROCK,			// いわ
+			METAL,			// はがね
+			GROUND,			// じめん
+			POISON,			// どく
+			HEEL,			// あく
+			PSYCHIC,		// エスパー
+			GOAST,			// ゴースト
+			DRAGON,			// ドラゴン
+			FAILY,			// フェアリー
+			ICE,			// こおり
+			LEGEND,			// レジェンド？
+			STELLA,			// ステラ？
+		}
+
+	}
 }
