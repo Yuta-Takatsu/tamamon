0ee5abedc57d7a59fc4be1752ff0028837a6abdf
naonosuke8267 <naonosuke8267@gmail.com>
2024-10-13 03:52:12 +0900

    ResourceManager　置いただけ
    中身は空

---
+++ Assets/Tamamon/Framework/ResourcesLoad/ResourceManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+
+namespace Framework
+{
+    /// <summary>
+    /// リソース管理クラス
+    /// </summary>
+    public class ResourceManager : MonoBehaviourSingleton<ResourceManager>
+    {
+        // Start is called before the first frame update
+        public override void Awake()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void RequestLoad()
+        {
+        
+        }
+    }
+}
